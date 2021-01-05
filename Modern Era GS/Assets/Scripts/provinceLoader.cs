d4378ec4aa5edd5af27e4d45f4e83a18de73480b
kikispcmr <43969187+kikispcmr@users.noreply.github.com>
2021-01-05 16:16:30 +0200

    Edited XML loader

--- Modern Era GS/Assets/Scripts/provinceLoader.cs
+++ Modern Era GS/Assets/Scripts/provinceLoader.cs
@@ -8,12 +8,6 @@ using System.Xml.Linq; //Needed for XDocument
 
 public class provinceLoader : MonoBehaviour
 {
-    XDocument XMLprovinces;
-
-    IEnumerable<XElement> items;
-
-    int iteration = 0;
-
     public struct provinces
     {
         public provinces(int provID, string name)
@@ -21,50 +15,37 @@ public class provinceLoader : MonoBehaviour
             ProvID = provID;
             Name = name;
         }
-        public int ProvID { get;}
+        public int ProvID { get; }
         public string Name { get; }
 
         public override string ToString() => $"({ProvID},{Name})";
     }
 
-    
 
-      Dictionary<Color, provinces> provincesDic = new Dictionary<Color, provinces>();
 
-// Start is called before the first frame update
-void Start()
+    public Dictionary<string, provinces> provincesDic = new Dictionary<string, provinces>();
+
+    void Start()
     {
-         LoadXML();
-    }
+        int i;
+        TextAsset txtXmlAsset = Resources.Load<TextAsset>("provinces");
+        var doc = XDocument.Parse(txtXmlAsset.text);
 
-void LoadXML()
-{
-    XMLprovinces = XDocument.Load("Assets/XML Files/Provinces.xml");
-    items = XMLprovinces.Descendants("").Elements();
-
-    foreach (var item in items)
-    {
-        if (item.Parent.Attribute("provID").Value == iteration.ToString())
+        var allDict = doc.Element("provinces").Elements("provID");
+        foreach(var oneDict in allDict)
         {
-            provID = int.Parse(item.Parent.Attribute(“number”).Value);
-            name = item.Parent.Element(“name”).Value.Trim(); 
-            rgb = item.Parent.Element(“rgb”).Value.Trim();
+            var provinceName = oneDict.Elements("name");
+            var provinceColor = oneDict.Elements("rgb");
+            var provID = oneDict.Elements("provID");
 
-            /*Create a new Index in the List, which will be a new XMLData object and pass the previously assigned variables as arguments so they get assigned to the new object’s variables.*/
+            string first = provinceName.ToString();
+            string second = provinceColor.ToString();
+            int third = int.Parse(provID.ToString());
 
-            
+            provincesDic.Add(first, new provinces(third, second));
+            Debug.Log("Succesfully ran");
+            Debug.Log("Province at " + provincesDic["112,255,99"]);
         }
-        data.Add(new XMLData(provID, name, rgb));
-
-
-        /*To test and make sure the data has been applied to properly, print out the musicClip name from the data list’s current index. This will let us know if the objects in the list have been created successfully and if their variables have been assigned the right values.*/
-
-        
-
-        Debug.Log(data[iteration].dialogueText); 
-
-        iteration++; //increment the iteration by 1
     }
-}
 
 }
